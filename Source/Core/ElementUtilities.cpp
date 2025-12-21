39d8e6cafa9b5f9eabe24cadfdf5267b5aea1956
Harvey <74739443+h-arvs@users.noreply.github.com>
2025-12-21 18:29:31 +0000

    Support non dom search in related ElementUtilities functions

--- Source/Core/ElementUtilities.cpp
+++ Source/Core/ElementUtilities.cpp
@@ -49,7 +49,7 @@
 
 namespace Rml {
 
-Element* ElementUtilities::GetElementById(Element* root_element, const String& id)
+Element* ElementUtilities::GetElementById(Element* root_element, const String& id, bool include_non_dom_elements = false)
 {
 	// Breadth first search on elements for the corresponding id
 	typedef Queue<Element*> SearchQueue;
@@ -67,19 +67,19 @@ Element* ElementUtilities::GetElementById(Element* root_element, const String& i
 		}
 
 		// Add all children to search
-		for (int i = 0; i < element->GetNumChildren(); i++)
+		for (int i = 0; i < element->GetNumChildren(include_non_dom_elements); i++)
 			search_queue.push(element->GetChild(i));
 	}
 
 	return nullptr;
 }
 
-void ElementUtilities::GetElementsByTagName(ElementList& elements, Element* root_element, const String& tag, const String& stop_tag)
+void ElementUtilities::GetElementsByTagName(ElementList& elements, Element* root_element, const String& tag, const String& stop_tag, bool include_non_dom_elements = false)
 {
 	// Breadth first search on elements for the corresponding id
 	typedef Queue<Element*> SearchQueue;
 	SearchQueue search_queue;
-	for (int i = 0; i < root_element->GetNumChildren(); ++i)
+	for (int i = 0; i < root_element->GetNumChildren(include_non_dom_elements); ++i)
 		search_queue.push(root_element->GetChild(i));
 
 	while (!search_queue.empty())
@@ -93,18 +93,18 @@ void ElementUtilities::GetElementsByTagName(ElementList& elements, Element* root
 		if (stop_tag.empty() || element->GetTagName() != stop_tag)
 		{
 			// Add all children to search.
-			for (int i = 0; i < element->GetNumChildren(); i++)
+			for (int i = 0; i < element->GetNumChildren(include_non_dom_elements); i++)
 				search_queue.push(element->GetChild(i));
 		}
 	}
 }
 
-void ElementUtilities::GetElementsByClassName(ElementList& elements, Element* root_element, const String& class_name)
+void ElementUtilities::GetElementsByClassName(ElementList& elements, Element* root_element, const String& class_name, bool include_non_dom_elements = false)
 {
 	// Breadth first search on elements for the corresponding id
 	typedef Queue<Element*> SearchQueue;
 	SearchQueue search_queue;
-	for (int i = 0; i < root_element->GetNumChildren(); ++i)
+	for (int i = 0; i < root_element->GetNumChildren(include_non_dom_elements); ++i)
 		search_queue.push(root_element->GetChild(i));
 
 	while (!search_queue.empty())
@@ -116,7 +116,7 @@ void ElementUtilities::GetElementsByClassName(ElementList& elements, Element* ro
 			elements.push_back(element);
 
 		// Add all children to search.
-		for (int i = 0; i < element->GetNumChildren(); i++)
+		for (int i = 0; i < element->GetNumChildren(include_non_dom_elements); i++)
 			search_queue.push(element->GetChild(i));
 	}
 }
